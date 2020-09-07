5fa27c6640bcb56776d6fb30ed542f6cbf59684b
J-P Nurmi <jpnurmi@gmail.com>
2020-09-07 08:25:33 +0200

    Add battery_plus_linux
    Close: #4

---
+++ battery_plus_linux/test/battery_plus_linux_test.dart
@@ -0,0 +1,33 @@
+import 'package:battery_plus_linux/src/battery_linux.dart';
+import 'package:battery_plus_linux/src/upower_device.dart';
+import 'package:battery_plus_platform_interface/battery_plus_platform_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDevice extends Mock implements UPowerDevice {}
+
+void main() {
+  test('battery level', () async {
+    final battery = BatteryLinux();
+    battery.createDevice = () {
+      final device = MockDevice();
+      when(device.getPercentage()).thenAnswer((_) => Future.value(56.78));
+      return device;
+    };
+    expect(battery.batteryLevel, completion(equals(57)));
+  });
+
+  test('battery state changes', () {
+    final battery = BatteryLinux();
+    battery.createDevice = () {
+      final device = MockDevice();
+      when(device.getState()).thenAnswer((_) {
+        return Future.value(UPowerBatteryState.charging);
+      });
+      return device;
+    };
+    battery.onBatteryStateChanged.listen(expectAsync1((state) {
+      expect(state, equals(BatteryState.charging));
+    }));
+  });
+}
